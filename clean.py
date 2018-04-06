1dae363b5ed2cc1ce3a3e43a960f32f79ad4649f
dJOKERdUMMY <avineshkumarcpl@gmail.com>
2018-04-06 14:25:26 +0530

    folder path manipulated, typos fixed

--- clean.py
+++ clean.py
@@ -1,9 +1,9 @@
 import os, shutil
 try:
-    if os.path.exists("train.zip"): os.remove("train.zip")
+    if os.path.exists("../train.zip"): os.remove("../train.zip")
     if os.path.exists("thyroid classification.zip"): os.remove("thyroid classification.zip")
 
-    shutil.rmtree("img_data", ignore_errors=True)
-    shutil.rmtree("data", ignore_errors=True)
+    shutil.rmtree("../img_data", ignore_errors=True)
+    shutil.rmtree("../data", ignore_errors=True)
 except:
     print("Some error might have come up. Please check the validity of the folders.")
--- transfer.py
+++ transfer.py
@@ -9,9 +9,11 @@ csv_data = pd.read_csv("csv_data/train.csv", usecols=['row_id', 'view_position',
 for i in range(int(len(csv_data)*0.7)):
     image_name = csv_data.loc[i,"image_name"]
     detected = csv_data.loc[i, "detected"]
+    view_position = str(csv_data.loc[i, "view_position"])
     curr_dir = os.getcwd()
+    curr_dir = os.path.normpath(curr_dir + os.sep + os.pardir)
     src = os.path.join(curr_dir, "data", image_name)
-    dest = os.path.join(curr_dir, "img_data/train", detected, image_name)
+    dest = os.path.join(curr_dir, "img_data/train", view_position+detected, image_name)
     shutil.copy(src, dest)
     if(i%300 == 0):
         print(i," files have been copied to train folder.")
@@ -19,11 +21,13 @@ for i in range(int(len(csv_data)*0.7)):
 for i in range(int(len(csv_data)*0.7), int(len(csv_data))):
     image_name = csv_data.loc[i,"image_name"]
     detected = csv_data.loc[i, "detected"]
+    view_position = str(csv_data.loc[i, "view_position"])
     curr_dir = os.getcwd()
+    curr_dir = os.path.normpath(curr_dir + os.sep + os.pardir)
     src = os.path.join(curr_dir, "data", image_name)
-    dest = os.path.join(curr_dir, "img_data/validation", detected, image_name)
+    dest = os.path.join(curr_dir, "img_data/validation", view_position+detected, image_name)
     shutil.copy(src, dest)
     if(i%300 == 0):
         print(i - int(len(csv_data)*0.7)," files have been copied to validation folder.")
 
-shutil.rmtree("data")
+shutil.rmtree("../data")
