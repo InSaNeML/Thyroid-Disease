2e6ed46522c98c5761cb44b247c7bcb398608a8d
dJOKERdUMMY <avineshkumarcpl@gmail.com>
2018-04-07 12:48:06 +0530

    typos fixed

--- src/main.py
+++ src/main.py
@@ -20,21 +20,21 @@ train_datagen = ImageDataGenerator(rescale = 1.0/255,
 	width_shift_range=0.2,
 	height_shift_range=0.2,
 	shear_range=0.2,
-	zoom_range=0.3,
+	zoom_range=0.2,
 	horizontal_flip=True,
 	fill_mode='nearest')
 
 validation_datagen = ImageDataGenerator(rescale = 1.0/255)
 
 train_generator = train_datagen.flow_from_directory(train_dir,
-	target_size = (512, 512),
+	target_size = (128, 128),
 	color_mode = "grayscale",
 	shuffle = "True",
 	batch_size = 1024,
 	class_mode = "categorical")
 
 validation_generator = validation_datagen.flow_from_directory(validation_dir,
-	target_size = (512, 512),
+	target_size = (128, 128),
 	color_mode = "grayscale",
 	shuffle = "True",
 	batch_size = 1024,
@@ -42,11 +42,11 @@ validation_generator = validation_datagen.flow_from_directory(validation_dir,
 
 #epochs = input("Enter number of epochs you want to train the model on:")
 
-epochs = 1
+epochs = 20
 epochs = int(epochs)
 print("Fitting data to Conv2d D model.")
 history = model.fit_generator(train_generator,
-	steps_per_epoch= 50,
+	#steps_per_epoch= 50,
 	epochs = epochs,
 	validation_data = validation_generator)
 
